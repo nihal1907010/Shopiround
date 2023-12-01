b28dba2d2acf43cca8ab97a9ee0cb6160db39955
Md. Mubtashim Abrar Nihal <mubtashim32@outlook.com>
2023-12-01 15:29:07 +0600

    Shop controller modifed

--- Shopiround/Areas/Shopkeeper/Controllers/ShopController.cs
+++ Shopiround/Areas/Shopkeeper/Controllers/ShopController.cs
@@ -41,7 +41,15 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
 
             ViewData["ProductCount"] = topProductCounts;
             ViewData["KeywordCount"] = MostSearchedKeyword;
-
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return View();
         }
 
@@ -53,7 +61,15 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             .ToList();
 
             ViewData["ProductCount"] = topProductCounts;
-
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return View();
         }
 
@@ -65,7 +81,15 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             .ToList();
 
             ViewData["ProductCount"] = topProductCounts;
-
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return View();
         }
         public IActionResult ShowAllKeywords()
@@ -75,7 +99,15 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             .ToList();
 
             ViewData["KeywordCount"] = MostSearchedKeyword;
-
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return View();
         }
         [Authorize]
@@ -90,6 +122,15 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             {
                 OwnerName = applicationUser.Name
             };
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return View(viewModel);
         }
         [HttpPost]
@@ -115,7 +156,126 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             applicationUser.Shop = shop;
             _unitOfWork.ApplicationUserRepository.Update(applicationUser);
             _unitOfWork.Save();
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            ViewBag.user = user;
+            ViewBag.shop = shop;
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Profile()
+        {
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == User.Identity.Name, includeProperties: "Shop");
+            Shop shop = applicationUser.Shop;
+            List<Product> products = context.Products.Where(p => p.ShopId == shop.ShopId).ToList();
+
+            ShopProfile shopProfile = new ShopProfile
+            {
+                shop = shop,
+                products = products
+            };
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            ViewBag.user = user;
+            ViewBag.shop = shop;
+            return View(shopProfile);
+        }
+        public IActionResult ShowUserInformation(string userId)
+        {
+            ApplicationUser user = _unitOfWork.ApplicationUserRepository.Get(u => u.Id == userId);
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == User.Identity.Name, includeProperties: "Shop");
+            UserProfileVM userProfile = new UserProfileVM
+            {
+                UserId = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                MobileNo = user.MobileNo,
+                ImageURL = user.ImageURL,
+                Address = user.Address
+            };
+            DeliveryInformation delivery = context.DeliveryInformation.Include(d => d.CartItem).Where(d => d.CartItem.UserId == user.Id).FirstOrDefault();
+            ViewBag.deliveryInfo = delivery;
+            // User and Shop information
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == applicationUser.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
+            return View(userProfile);
+        }
+
+        public IActionResult OnlineOrdersHome()
+        {
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == User.Identity.Name, includeProperties: "Shop");
+            List<Product> products = context.Products.Where(p => p.ShopId == applicationUser.Shop.ShopId).ToList();
+            List<CartItem> cartItems = context.CartItems.Include(c => c.Product).Include(c => c.User).Where(c => products.Contains(c.Product) && c.Online).ToList();
+            List<OnlineOrderVM> onlineOrders = (from cart in cartItems
+                                                group cart by cart.UserId into g
+                                                select new OnlineOrderVM
+                                                {
+                                                    User = context.ApplicationUsers.Where(u => u.Id == g.Key).First(),
+                                                    TotalOrders = g.Count(),
+                                                    TotalPrice = (int)g.Sum(c => c.Product.Price)
+                                                }).ToList();
+            onlineOrders.Reverse();
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
+            return View(onlineOrders);
+        }
+        [Authorize]
+        public IActionResult DoneOnlineShopping(string userId)
+        {
+            ApplicationUser user = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == userId, includeProperties: "Shop,CartItems");
+            List<CartItem> cartItems = context.CartItems.Include(c => c.Product).ThenInclude(s => s.Shop).Where(c => c.UserId == userId && c.Online).ToList();
+            foreach (CartItem cartItem in cartItems)
+            {
+                PurchaseItem purchaseItem = new PurchaseItem
+                {
+                    UserId = cartItem.UserId,
+                    ProductId = cartItem.ProductId,
+                    Quantity = cartItem.Quantity,
+                    Online = cartItem.Online,
+                    Reserve = cartItem.Reserve,
+                    PurchaseDate = DateTime.Now
+                };
+                context.PurchaseItems.Add(purchaseItem);
+            }
+            context.CartItems.RemoveRange(cartItems);
+            context.SaveChanges();
+            // User and Shop information
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult OnlineOrder(string userId)
+        {
+            List<CartItem> cartItems = context.CartItems.Where(c => c.Online).Include(c => c.Product).ThenInclude(s => s.Shop).Where(c => c.UserId == userId).ToList();
+            ViewBag.userId = userId;
+            // User and Shop information
+            ApplicationUser? user = context.ApplicationUsers.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            Shop? shop = null;
+            if (user != null)
+            {
+                shop = context.Shops.Where(x => x.ApplicationUserId == user.Id).FirstOrDefault();
+            }
+            ViewBag.user = user;
+            ViewBag.shop = shop;
+            return View(cartItems);
         }
 
         public IActionResult Profile(int? id)
@@ -150,56 +310,7 @@ namespace Shopiround.Areas.Shopkeeper.Controllers
             }
            
         }
-        public IActionResult ShowUserInformation()
-        {
-            return View();
-        }
 
-        public IActionResult OnlineOrdersHome()
-        {
-            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == User.Identity.Name, includeProperties: "Shop");
-            List<Product> products = context.Products.Where(p => p.ShopId == applicationUser.Shop.ShopId).ToList();
-            List<CartItem> cartItems = context.CartItems.Include(c => c.Product).Include(c => c.User).Where(c => products.Contains(c.Product) && c.Online).ToList();
-            List<OnlineOrderVM> onlineOrders = (from cart in cartItems
-                    group cart by cart.UserId into g
-                    select new OnlineOrderVM
-                    {
-                        User = context.ApplicationUsers.Where(u => u.Id == g.Key).First(),
-                        TotalOrders = g.Count(),
-                        TotalPrice = (int)g.Sum(c => c.Product.Price)
-                    }).ToList();
-            onlineOrders.Reverse();
-            return View(onlineOrders);
-        }
-        [Authorize]
-        public IActionResult DoneOnlineShopping(string userId)
-        {
-            ApplicationUser user = _unitOfWork.ApplicationUserRepository.Get(u => u.UserName == userId, includeProperties: "Shop,CartItems");
-            List<CartItem> cartItems = context.CartItems.Include(c => c.Product).ThenInclude(s => s.Shop).Where(c => c.UserId == userId && c.Online).ToList();
-            foreach (CartItem cartItem in cartItems)
-            {
-                PurchaseItem purchaseItem = new PurchaseItem
-                {
-                    UserId = cartItem.UserId,
-                    ProductId = cartItem.ProductId,
-                    Quantity = cartItem.Quantity,
-                    Online = cartItem.Online,
-                    Reserve = cartItem.Reserve,
-                    PurchaseDate = DateTime.Now
-                };
-                context.PurchaseItems.Add(purchaseItem);
-            }
-            context.CartItems.RemoveRange(cartItems);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult OnlineOrder(string userId)
-        {
-            List<CartItem> cartItems = context.CartItems.Where(c => c.Online).Include(c => c.Product).ThenInclude(s => s.Shop).Where(c => c.UserId == userId).ToList();
-            ViewBag.userId = userId;
-            return View(cartItems);
-        }
 
         public IActionResult ShopNearby()
         {
